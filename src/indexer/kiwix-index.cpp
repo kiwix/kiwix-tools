f3bd14cf83ef09945ac096da9faa8c28699abd19
kelson42 <kelson42@users.sourceforge.net>
2009-12-20 14:10:58 +0000

    + few imp. in the indexer code

--- src/indexer/kiwix-index.cpp
+++ src/indexer/kiwix-index.cpp
@@ -11,14 +11,21 @@ int main(int argc, char **argv) {
   /* Init the variables */
   char *zimFilePath = argv[1];
   char *xapianDirectoryPath = argv[2];
-  kiwix::Indexer *indexer = new kiwix::Indexer(zimFilePath, xapianDirectoryPath);
+  kiwix::Indexer *indexer = NULL;
+
+  /* Try to prepare the indexing */
+  try {
+    indexer = new kiwix::Indexer(zimFilePath, xapianDirectoryPath);
+  } catch (...) {
+    cout << "Unable to index '" << zimFilePath << "'." << endl;
+    exit(1);
+  }
 
   /* Start the indexing */
   if (indexer != NULL) {
-    indexer->startIndexing();
     while (indexer->indexNextPercent()) {};
   } else {
-    cout << "Unable to start the indexation process" << endl;
+    cout << "Unable instanciate the Kiwix indexer." << endl;
     exit(1);
   }
 
