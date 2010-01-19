87209689dbe415c3fe8baf08d310717bdeb2aedc
kelson42 <kelson42@users.sourceforge.net>
2010-01-19 11:57:18 +0000

    + static HTML to modify on the fly the HTML pages

--- src/server/kiwix-serve.cpp
+++ src/server/kiwix-serve.cpp
@@ -13,9 +13,115 @@
 #include <zim/file.h>
 #include <zim/article.h>
 #include <zim/fileiterator.h>
-#include<pthread.h>
+#include <pthread.h>
+#include <regex>
 
 using namespace std;
+
+static const string HTMLScripts = " \
+<style type=\"text/css\"> \
+ \
+#topbar{ \
+position:absolute; \
+border: 1px solid black; \
+padding: 2px; \
+background-color: lightyellow; \
+width: 620px; \
+visibility: hidden; \
+z-index: 100; \
+} \
+ \
+</style> \
+ \
+<script type=\"text/javascript\"> \
+ \
+/*********************************************** \
+* Floating Top Bar script- Â Dynamic Drive (www.dynamicdrive.com) \
+* Sliding routine by Roy Whittle (http://www.javascript-fx.com/) \
+* This notice must stay intact for legal use. \
+* Visit http://www.dynamicdrive.com/ for full source code \
+***********************************************/ \
+ \
+var persistclose=0 //set to 0 or 1. 1 means once the bar is manually closed, it will remain closed for browser session \
+var startX = 30 //set x offset of bar in pixels \
+var startY = 5 //set y offset of bar in pixels \
+var verticalpos=\"fromtop\" //enter \"fromtop\" or \"frombottom\" \
+ \
+function iecompattest(){ \
+return (document.compatMode && document.compatMode!=\"BackCompat\")? document.documentElement : document.body \
+} \
+ \
+function get_cookie(Name) { \
+var search = Name + \"=\" \
+var returnvalue = \"\"; \
+if (document.cookie.length > 0) { \
+offset = document.cookie.indexOf(search) \
+if (offset != -1) { \
+offset += search.length \
+end = document.cookie.indexOf(\";\", offset); \
+if (end == -1) end = document.cookie.length; \
+returnvalue=unescape(document.cookie.substring(offset, end)) \
+} \
+} \
+return returnvalue; \
+} \
+ \
+function closebar(){ \
+if (persistclose) \
+document.cookie=\"remainclosed=1\" \
+document.getElementById(\"topbar\").style.visibility=\"hidden\" \
+} \
+ \
+function staticbar(){ \
+barheight=document.getElementById(\"topbar\").offsetHeight \
+var ns = (navigator.appName.indexOf(\"Netscape\") != -1) || window.opera; \
+var d = document; \
+function ml(id){ \
+var el=d.getElementById(id); \
+if (!persistclose || persistclose && get_cookie(\"remainclosed\")==\"\") \
+el.style.visibility=\"visible\" \
+if(d.layers)el.style=el; \
+el.sP=function(x,y){this.style.left=x+\"px\";this.style.top=y+\"px\";}; \
+el.x = startX; \
+if (verticalpos==\"fromtop\") \
+el.y = startY; \
+else{ \
+el.y = ns ? pageYOffset + innerHeight : iecompattest().scrollTop + iecompattest().clientHeight; \
+el.y -= startY; \
+} \
+return el; \
+} \
+window.stayTopLeft=function(){ \
+if (verticalpos==\"fromtop\"){ \
+var pY = ns ? pageYOffset : iecompattest().scrollTop; \
+ftlObj.y += (pY + startY - ftlObj.y)/8; \
+} \
+else{ \
+var pY = ns ? pageYOffset + innerHeight - barheight: iecompattest().scrollTop + iecompattest().clientHeight - barheight; \
+ftlObj.y += (pY - startY - ftlObj.y)/8; \
+} \
+ftlObj.sP(ftlObj.x, ftlObj.y); \
+setTimeout(\"stayTopLeft()\", 10); \
+} \
+ftlObj = ml(\"topbar\"); \
+stayTopLeft(); \
+} \
+ \
+if (window.addEventListener) \
+window.addEventListener(\"load\", staticbar, false) \
+else if (window.attachEvent) \
+window.attachEvent(\"onload\", staticbar) \
+else if (document.getElementById) \
+window.onload=staticbar \
+</script> \
+";
+
+static const string HTMLDiv = " \
+<div id=\"topbar\"> \
+<a href=\"\" onClick=\"closebar(); return false\"><img src=\"close.gif\" border=\"0\" /></a> \
+Your content here. \
+</div> \
+";
 
 static zim::File* zimFileHandler;
 static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
