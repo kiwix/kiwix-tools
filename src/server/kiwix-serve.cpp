449e1346f0083cc1cd700f35b8e6d22280b42ab7
kelson42 <kelson42@users.sourceforge.net>
2010-01-24 12:12:37 +0000

    + small bug fix (search box was not displayed with HTML mime-type containing a charset)

--- src/server/kiwix-serve.cpp
+++ src/server/kiwix-serve.cpp
@@ -192,7 +192,7 @@ static int accessHandlerCallback(void *cls,
   }
   
   /* Rewrite the content (add the search box) */
-  if (hasSearchIndex && mimeType == "text/html") {
+  if (hasSearchIndex && mimeType.find("text/html")) {
     appendToFirstOccurence(content, "<head>", HTMLScripts);
     appendToFirstOccurence(content, "<body[^>]*>", HTMLDiv);
     contentLength = content.size();
