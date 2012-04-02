bac4677edcee3976ccc2e21c7a14da829383d9ad
kelson42 <kelson42@users.sourceforge.net>
2012-04-02 19:49:16 +0000

    + fix small bug in the new indexer

--- src/indexer/kiwix-index.cpp
+++ src/indexer/kiwix-index.cpp
@@ -107,14 +107,11 @@ int main(int argc, char **argv) {
 
   /* Start the indexing */
   if (indexer != NULL) {
-    /*
     indexer->start();
     while (indexer->isRunning()) {
-      cout << "Checking if running..." << endl;
       sleep(1);
     }
-    */
-    while (indexer->indexNextPercent(verboseFlag)) {};
+    //while (indexer->indexNextPercent(verboseFlag)) {};
     delete indexer;
   } else {
     cerr << "Unable instanciate the Kiwix indexer." << endl;
