90c1e137c88d8e35f32f9d1cf55417f1ec817243
kelson42 <kelson42@users.sourceforge.net>
2013-06-17 14:40:16 +0200

    + In the homepage, replace form method POST by GET (allow page reload without popup)

--- src/server/kiwix-serve.cpp
+++ src/server/kiwix-serve.cpp
@@ -579,7 +579,7 @@ int main(int argc, char **argv) {
                              <td style=\"width: 100%;\">" + currentBook.description +
 	                       "<br/><table style=\"font-size: small; color: grey; width: 100%;\">" +
 	"<tr><td style=\"width: 50%\">Size: " + kiwix::beautifyInteger(atoi(currentBook.size.c_str())) + " MB (" + kiwix::beautifyInteger(atoi(currentBook.articleCount.c_str())) + " articles, " + kiwix::beautifyInteger(atoi(currentBook.mediaCount.c_str())) + " medias) \
-                                  </td><td>Created: " + currentBook.date + "</td><td style=\"vertical-align: bottom;\" rowspan=\"3\"><form action=\"/" + currentBook.getHumanReadableIdFromPath() + "/\" method=\"POST\"><input style=\"align: right; right: 0px; float:right;\" type=\"submit\" value=\"Load\" /></form></td></tr>					\
+                                  </td><td>Created: " + currentBook.date + "</td><td style=\"vertical-align: bottom;\" rowspan=\"3\"><form action=\"/" + currentBook.getHumanReadableIdFromPath() + "/\" method=\"GET\"><input style=\"align: right; right: 0px; float:right;\" type=\"submit\" value=\"Load\" /></form></td></tr>					\
                                   <tr><td>Author: " + currentBook.creator + "</td><td>Language: " + currentBook.language + "</td></tr> \
                                   <tr><td>Publisher: " + (currentBook.publisher.empty() ? "unknown" :  currentBook.publisher ) + "</td><td></td></tr> \
                                 </table> \
