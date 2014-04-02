ef6d5bc0552f6c71a814e9dd181cb44398434213
kelson42 <kelson@kiwix.org>
2014-04-02 15:36:37 +0200

    +  update coypright header

--- src/server/kiwix-serve.cpp
+++ src/server/kiwix-serve.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2012 Emmanuel Engelhart <kelson@kiwix.org>
+ * Copyright 2009-2014 Emmanuel Engelhart <kelson@kiwix.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU  General Public License as published by
