6f2316bc8aa673a81e843a7f285b9c48ad952cc4
arjun-369 <68965236+arjun-369@users.noreply.github.com>
2021-04-07 17:32:07 +0530

    Update kiwix-search.cpp for a bug fix

--- src/searcher/kiwix-search.cpp
+++ src/searcher/kiwix-search.cpp
@@ -104,7 +104,14 @@ int main(int argc, char** argv)
 
   if (reader) {
     searcher = new kiwix::Searcher();
-    searcher->add_reader(reader);
+    bool contians_FTIndex=searcher->add_reader(reader);
+    if(!contians_FTIndex){
+        std::cerr << "The Zim file does not contain a full-text index." << std::endl;
+        if(suggestionFlag){
+            exit(0);
+        }
+        exit(1);
+      }
   } else {
     cerr << "Unable to search through zim '" << zimPath << "'." << endl;
     exit(1);
